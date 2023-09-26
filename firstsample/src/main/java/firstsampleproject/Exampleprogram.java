37292438c17b129a111af983bc1fd2b8f7674f66
Stebin <stebin@thecodelab.me>
2023-09-26 15:12:27 +0530

    second commit

--- firstsample/src/main/java/firstsampleproject/Exampleprogram.java
+++ firstsample/src/main/java/firstsampleproject/Exampleprogram.java
@@ -5,6 +5,7 @@ public class Exampleprogram {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello");
+		System.out.println("Hello");
 		// TODO Auto-generated method stub
 
 	}
